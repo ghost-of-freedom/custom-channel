69abcc1895b857271b056ed180e2b67edef30743
name <no@example.com>
2022-07-11 21:25:06 +0200

    Add dhcpcd

---
+++ admin.scm
@@ -0,0 +1,47 @@
+(define-module (admin)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages cups)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gawk)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages libbsd)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages mpd)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
+  #:use-module (guix packages))
+
+(define-public custom-dhcpcd
+  (package
+   (name "custom-dhcpcd")
+   (version "9.4.1")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/NetworkConfiguration/dhcpcd.git")
+           (commit "3c458fc7fa4146029a1e4f9e98cd7e7adf03081a")))
+     (file-name (git-file-name name version)) 
+     (sha256
+      (base32 "0yvm7v56jl5xsy56ks12g5cwc8rfhcxbxsnpsp94x0z3xh9q5f4n")))) 
+   (build-system gnu-build-system)
+   (arguments
+    `(#:tests? #f))
+   (home-page "https://roy.marples.name/projects/dhcpcd/")
+   (synopsis "DHCP / IPv4LL / IPv6RA / DHCPv6 client.")
+   (description "dhcpcd is a DHCP and a DHCPv6 client. It's also an IPv4LL (aka ZeroConf) client. In layman's terms, dhcpcd runs on your machine and silently configures your computer to work on the attached networks without trouble and mostly without configuration.")
+   (license license:bsd-2)))
