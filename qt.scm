930641aa838731cac7e4cfec5f35c01e9efdb096
name <no@example.com>
2022-07-16 11:11:58 +0200

    Try qt-style-plugins

---
+++ qt.scm
@@ -0,0 +1,28 @@
+(define-module (qt)
+  #:use-module (guix licenses)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix build-system qt))
+
+(define-public custom-qt-style-plugins
+  (package
+   (name "custom-qt-style-plugins")
+   (version "5.0.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/ghost-of-freedom/qtstyleplugins")
+           (commit "335dbece103e2cbf6c7cf819ab6672c2956b17b3")))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "1razd3xm4s4dggmns81cdv7xzpi65igxqlqjc1y2gg6nf99923rg"))))
+   (build-system qt-make-system)
+   (inputs (list qt5-base gtk libx11 pango))
+   (native-inputs (list qt5-base gtk libx11 pango))
+   (synopsis "Additional style plugins for Qt")
+   (description "Additional style plugins for Qt")
+   (home-page "https://github.com/qt/qtstyleplugins")
+   (license lgpl2.1)))
